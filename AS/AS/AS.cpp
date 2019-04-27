38b68dffe9dd4f4c36d4318433dc83bd67dad41b
foXerw <34823097+foXerw@users.noreply.github.com>
2019-04-27 20:38:20 +0800

    不会就我一个vs2019吧

---
+++ AS/AS/AS.cpp
@@ -0,0 +1,19 @@
+#include "AS.h"
+
+string ASserver::GetTicketTGS(string KeyCTGS, string IDC, string CAddr, string IDTGS, int LifeTime2)
+//封装加密生成Client与TGS验证所需要的TicketTGS
+{
+
+}
+
+string ASserver::AS_CDataEncapsulation(string TicketTGS, string KeyCTGS, string IDTGS, int LifeTime2)
+//封装加密整合生成最终要发回Client的数据包
+{
+
+}
+
+string ASserver::AS_CDataDeEncapsulation()
+//将Client发来的数据包进行解封装
+{
+	
+}
---
+++ Client/Client/Client.cpp
@@ -0,0 +1,37 @@
+#include "Client.h"
+
+string Client::C_ASDataEncapsulation(string IDTGS)
+//AS通信数据封装函数，根据Client向AS所需发送的数据进行封装加密
+{
+
+}
+
+string Client::C_ASDataDeEncapsulation()
+//AS通信数据解封装函数，根据AS发来的数据包，进行解密拆分
+{
+
+}
+
+string Client::C_TGSDataEncapsulation(string IDV, string TicketTGS)
+//TGS通信数据封装函数，根据Client向TGS所需发送的数据进行封装加密
+{
+
+}
+
+string Client::C_TGSDataDeEncapsulation()
+//TGS通信数据解封装函数，根据TGS发来的数据包，进行解密拆分
+{
+
+}
+
+string Client::C_VDataEncapsulation(string TicketV)
+//客户服务器通信数据封装函数，根据Client向服务器所需发送的数据进行封装加密
+{
+
+}
+
+string Client::C_VDataDeEncapsulation()
+//客户服务器通信数据解封装函数，根据TGS发来的数据包，进行解密拆分
+{
+
+}
